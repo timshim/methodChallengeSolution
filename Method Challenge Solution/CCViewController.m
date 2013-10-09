08eff7f1b12ea61b972e3182552e70a441ab25c0
timshim <timshim@gmail.com>
2013-10-10 00:52:32 +0800

    First commit

--- Method Challenge Solution/CCViewController.m
+++ Method Challenge Solution/CCViewController.m
@@ -18,6 +18,40 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    int result = [self factorialOfNumber:5];
+    NSLog(@"%i", result);
+}
+
+- (void)printWholeNumbersBelow:(int)number
+{
+    for (int i = number; i > 0; i--)
+    {
+        NSLog(@"%i", i);
+    }
+}
+
+- (void)printWholeNumbersFrom:(int)firstNumber toNumber:(int)secondNumber
+{
+    if (firstNumber == secondNumber) {
+        NSLog(@"The numbers are equal!");
+    } else if (firstNumber > secondNumber) {
+        for (int i = firstNumber; i >= secondNumber; i--) {
+            NSLog(@"%i", i);
+        }
+    } else {
+        for (int i = secondNumber; i >= firstNumber; i--) {
+            NSLog(@"%i", i);
+        }
+    }
+}
+
+- (int)factorialOfNumber:(int)number
+{
+    int answer = number;
+    for (int i = number; i > 1; i--) {
+        answer = answer * (i - 1);
+    }
+    return answer;
 }
 
 - (void)didReceiveMemoryWarning
